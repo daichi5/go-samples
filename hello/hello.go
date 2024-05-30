c9dbdc460aa7c9bd796cbab0e302548137b7a209
daichi5 <sawa22daichi@gmail.com>
2024-05-30 10:22:58 +0900

    add error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
+
+	fmt.Println("finished.")
 }
