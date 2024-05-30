a847c549be8a1833662410d9460f51b0d7b330c8
daichi5 <sawa22daichi@gmail.com>
2024-05-30 09:53:52 +0900

    use greetings module

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
