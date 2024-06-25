a9de14f6c36a77affca710ad803546916cf6cb64
daichi5 <sawa22daichi@gmail.com>
2024-06-25 22:41:21 +0900

    add fuzz test

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"unicode/utf8"
+)
 
 func TestReverse(t *testing.T) {
 	testCases := []struct {
@@ -18,4 +22,26 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		fmt.Println(orig)
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString((rev)) {
+			t.Errorf("Reverse produced invalid UTF-8 string: %q", rev)
+		}
+	})
 }
