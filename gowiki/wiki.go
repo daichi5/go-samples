38f6ed95814db0014a798d79202ddd39370c3611
daichi5 <sawa22daichi@gmail.com>
2024-06-29 14:36:56 +0900

    add error handling

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -25,8 +25,19 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, p)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,6 +58,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
+
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,7 +67,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 
 	p := &Page{Title: title, Body: []byte(body)}
 
-	p.save()
+	err := p.save()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 
