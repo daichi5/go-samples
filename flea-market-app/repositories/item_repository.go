84ecbf8d0d696b1b44bdfa08c8156845987913a2
daichi5 <sawa22daichi@gmail.com>
2024-12-30 15:44:52 +0900

    add ItemRepository

---
+++ flea-market-app/repositories/item_repository.go
@@ -0,0 +1,21 @@
+package repositories
+
+import "flea-market-app/models"
+
+type IItemRepository interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type ItemMemoryRepository struct {
+	items []models.Item
+}
+
+func NewItemMemoryRepository(items []models.Item) IItemRepository {
+	return &ItemMemoryRepository{
+		items: items,
+	}
+}
+
+func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
+	return &r.items, nil
+}
