b8f0c8f1f8fbe9ac99bc2096aed733ba6bb3f1b7
daichi5 <sawa22daichi@gmail.com>
2024-12-31 15:20:06 +0900

    add items endpoint

--- flea-market-app/main.go
+++ flea-market-app/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"flea-market-app/controllers"
+	"flea-market-app/models"
+	"flea-market-app/repositories"
+	"flea-market-app/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "Item1", Price: 1000, Description: "Description1", SoldOut: false},
+		{ID: 2, Name: "Item2", Price: 2000, Description: "Description2", SoldOut: true},
+		{ID: 3, Name: "Item3", Price: 1000, Description: "Description3", SoldOut: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/items", itemController.FindAll)
+	r.GET("/items/:id", itemController.FindById)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
