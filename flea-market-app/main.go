0ad57acb9f347baffe2376dac2e5a4eb39f3ea2f
daichi5 <sawa22daichi@gmail.com>
2025-01-03 00:07:00 +0900

    add PUT items/:id endpoint

--- flea-market-app/main.go
+++ flea-market-app/main.go
@@ -24,6 +24,7 @@ func main() {
 	r.GET("/items", itemController.FindAll)
 	r.GET("/items/:id", itemController.FindById)
 	r.POST("/items", itemController.Create)
+	r.PUT("/items/:id", itemController.Update)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
