fd7ea46c8989eb3d9a5297493698a561593e9200
daichi5 <sawa22daichi@gmail.com>
2025-01-08 21:24:53 +0900

    create ItemRepository

--- flea-market-app/main.go
+++ flea-market-app/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"flea-market-app/controllers"
 	"flea-market-app/infra"
-	"flea-market-app/models"
+
+	// "flea-market-app/models"
 	"flea-market-app/repositories"
 	"flea-market-app/services"
 
@@ -13,13 +14,16 @@ import (
 func main() {
 	infra.Initialize()
 
-	items := []models.Item{
-		{ID: 1, Name: "Item1", Price: 1000, Description: "Description1", SoldOut: false},
-		{ID: 2, Name: "Item2", Price: 2000, Description: "Description2", SoldOut: true},
-		{ID: 3, Name: "Item3", Price: 1000, Description: "Description3", SoldOut: false},
-	}
+	db := infra.SetupDB()
 
-	itemRepository := repositories.NewItemMemoryRepository(items)
+	// items := []models.Item{
+	// 	{ID: 1, Name: "Item1", Price: 1000, Description: "Description1", SoldOut: false},
+	// 	{ID: 2, Name: "Item2", Price: 2000, Description: "Description2", SoldOut: true},
+	// 	{ID: 3, Name: "Item3", Price: 1000, Description: "Description3", SoldOut: false},
+	// }
+
+	// itemRepository := repositories.NewItemMemoryRepository(items)
+	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
--- flea-market-app/repositories/item_repository.go
+++ flea-market-app/repositories/item_repository.go
@@ -3,6 +3,8 @@ package repositories
 import (
 	"errors"
 	"flea-market-app/models"
+
+	"gorm.io/gorm"
 )
 
 type IItemRepository interface {
@@ -64,4 +66,56 @@ func (r *ItemMemoryRepository) Delete(itemId uint) error {
 		}
 	}
 	return errors.New("item not found")
+}
+
+type ItemRepository struct {
+	db *gorm.DB
+}
+
+func (r *ItemRepository) Create(newItem models.Item) (*models.Item, error) {
+	result := r.db.Create(&newItem)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &newItem, nil
+}
+
+// Delete implements IItemRepository.
+func (i *ItemRepository) Delete(itemId uint) error {
+	panic("unimplemented")
+}
+
+// FindAll implements IItemRepository.
+func (r *ItemRepository) FindAll() (*[]models.Item, error) {
+	var items []models.Item
+	result := r.db.Find(&items)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &items, nil
+}
+
+// FindById implements IItemRepository.
+func (r *ItemRepository) FindById(itemId uint) (*models.Item, error) {
+	var item models.Item
+	result := r.db.First(&item, itemId)
+	if result.Error != nil {
+		if result.Error.Error() == "record not found" {
+			return nil, errors.New("item not found")
+		}
+		return nil, result.Error
+	}
+
+	return &item, nil
+}
+
+// Update implements IItemRepository.
+func (i *ItemRepository) Update(updateItem models.Item) (*models.Item, error) {
+	panic("unimplemented")
+}
+
+func NewItemRepository(db *gorm.DB) IItemRepository {
+	return &ItemRepository{db: db}
 }
